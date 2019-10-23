44bc5853539878f2c7c4d4439f474ff87f449cb2
lucamatsumoto <lucamatsumoto@gmail.com>
2019-10-23 11:15:55 -0700

    example model

---
+++ api/swipr/src/main/java/com/swipr/models/User.java
@@ -0,0 +1,22 @@
+package com.swipr.models;
+
+import org.springframework.*;
+import lombok.*;
+
+// potential user model example
+
+// @Entity we may need this if we use a DB
+@Getter @Setter @NoArgsConstructor
+public class User {
+
+    private String name;
+    private String email;
+    private String venmo;
+
+    public User(String name, String email, String venmo) {
+        this.name = name;
+        this.email = email;
+        this.venmo = venmo;
+    }
+    
+}
