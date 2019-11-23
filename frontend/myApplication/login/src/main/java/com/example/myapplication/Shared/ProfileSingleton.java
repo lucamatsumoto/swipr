5e06bdee69b745dc55fc4f741b31694be6fdf2a4
tholt1220 <trevholt1220@gmail.com>
2019-11-23 13:41:02 -0800

    Added function to represent user as a JSON object

--- frontend/myApplication/login/src/main/java/com/example/myapplication/Shared/ProfileSingleton.java
+++ frontend/myApplication/login/src/main/java/com/example/myapplication/Shared/ProfileSingleton.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.Shared;
 
+import android.net.Uri;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -12,6 +13,7 @@ public class ProfileSingleton {
     private static String m_lastName;
     private static String m_email;
     private static String m_venmo;
+    private static Uri m_profPicString;
     private static boolean here;
     private static boolean matchedOffer;
 
@@ -31,6 +33,7 @@ public class ProfileSingleton {
             m_venmo = checkNull(reader.getString("venmo"));
             here = Boolean.valueOf(reader.getString("here"));
             matchedOffer = Boolean.valueOf(reader.getString("matchedOffer"));
+            //m_profPicString =
             Log.d("Here", "profile created");
         }
         catch (JSONException e)
@@ -44,6 +47,7 @@ public class ProfileSingleton {
     public String getLastName() {return m_lastName;}
     public String getEmail() {return m_email;}
     public String getVenmo() {return m_venmo;}
+
     public boolean isHere() {return here;}
     public boolean hasMatched() {return matchedOffer;}
     public void setVenmo(String s) {m_venmo = s;}
@@ -57,5 +61,24 @@ public class ProfileSingleton {
             return null;
         else
             return s;
+    }
+
+    public static JSONObject asJSON()
+    {
+        JSONObject json = new JSONObject();
+        try
+        {
+            json.put("id", m_id);
+            json.put("firstName", m_firstName);
+            json.put("lastName", m_lastName);
+            json.put("email", m_email);
+            json.put("profilePicUrl", m_profPicString);
+            json.put("venmo", m_venmo);
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
+        return json;
     }
 }
