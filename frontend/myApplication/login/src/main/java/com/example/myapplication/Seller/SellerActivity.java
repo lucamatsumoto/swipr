c395f7ab5ae80d57c2642bfb8f6e6f05cc2c460b
Ryan Miyahara <rmiyahara144@gmail.com>
2019-11-26 18:37:08 -0800

    Completed Seller Activity

--- frontend/myApplication/login/src/main/java/com/example/myapplication/Seller/SellerActivity.java
+++ frontend/myApplication/login/src/main/java/com/example/myapplication/Seller/SellerActivity.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import com.innovattic.rangeseekbar.RangeSeekBar;
 
+import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.chip.Chip;
 import com.example.myapplication.Buyer.BuyerActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.Shared.DrawerBaseActivity;
@@ -16,7 +19,9 @@ import com.example.myapplication.Shared.DrawerBaseActivity;
 public class SellerActivity extends DrawerBaseActivity {
 
     SeekBar s_price;
-    SeekBar s_time;
+    RangeSeekBar s_time;
+    RangeSeekBar.SeekBarChangeListener L;
+    String TAG;
 
 
     @Override
@@ -33,17 +38,15 @@ public class SellerActivity extends DrawerBaseActivity {
         dl.addView(contentView, 0);
         //END
 
-        // initiate  views
-        setContentView(R.layout.activity_seller);
+        // Sliders
         int step_value_price = 50; //Number of cents to change the total by
         TextView textView_price = findViewById(R.id.price_value);
         s_price=findViewById(R.id.s_price);
         s_price.setMax(1500);
         // perform seek bar change listener event used for getting the progress value
         s_price.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progressChangedValue;
             {
-                progressChangedValue = 0;
+                s_price.setProgress(800);
                 textView_price.setText("$" + String.format("%.2f", s_price.getProgress() / (float) 100));
             }
 
@@ -62,31 +65,39 @@ public class SellerActivity extends DrawerBaseActivity {
 
             }
         });
-        int step_value_time = 30; //Number of cents to change the total by
+
+        //int step_value_time = 30; //Number of min per step
         s_time=findViewById(R.id.s_time);
         TextView textView_time = findViewById(R.id.time_value);
-        s_time.setMax(1439); //Minutes for 23:59 hours
+        s_time.setMax(47); //Minutes for 23:59 hours
+        s_time.setMinThumbValue(16);
+        s_time.setMaxThumbValue(24);
+        s_time.setMinRange(1);
         // perform seek bar change listener event used for getting the progress value
-        s_time.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progressChangedValue;
-            {
-                progressChangedValue = 0;
-                textView_time.setText((String.format("%d", s_time.getProgress() / 60)) + ":" + (String.format("%02d", s_time.getProgress() % 60)));            }
-
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progress = ((int)Math.round(progress/step_value_time )) * step_value_time;
-                seekBar.setProgress(progress);
-                textView_time.setText((String.format("%d", s_time.getProgress() / 60)) + ":" + (String.format("%02d", s_time.getProgress() % 60)));
+        textView_time.setText("8:00 - 12:00");
+        L = new RangeSeekBar.SeekBarChangeListener() {
+            @Override
+            public void onStartedSeeking() {
+                Log.i(TAG, "Started seeking.");
             }
 
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
+            @Override
+            public void onStoppedSeeking() {
+                Log.i(TAG, "Stopped seeking.");
             }
 
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            @Override
+            public void onValueChanged(int left_thumb, int right_thumb) {
+                int left_thumb_value = left_thumb * 30;
+                int right_thumb_value = right_thumb * 30;
+                String left_hours = String.format("%2d", left_thumb_value / 60);
+                String left_mins = String.format("%02d", left_thumb_value % 60);
+                String right_hours = String.format("%2d", right_thumb_value / 60);
+                String right_mins = String.format("%02d", right_thumb_value % 60);
+                textView_time.setText(left_hours + ":" + left_mins + " - " + right_hours + ":" + right_mins);
             }
-        });
+        };
+        s_time.setSeekBarChangeListener(L);
     }
 
     public void launchBuyerActivity(View view) {
