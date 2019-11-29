dc9c0705abdcc2f995940fe1070ae5902b6c5f63
tholt1220 <32722657+tholt1220@users.noreply.github.com>
2019-11-28 20:58:56 -0800

    Trevor frontend (#6)
    * Functioning Websockets on front end. Attempt at intergrating User and login views
    
    * Initial merge of Login and Buyer frontend.
    Have yet to workout why the Layout xmls know of String xmls
    in the other module.
    Have yet to delete Buyer Module.
    
    * Remove ignored files?
    
    * Moved Networking functionality to a singleton
    
    * Added functionality to ProfileSingleton
    Fixed Facebook Login
    
    * Added simple profile navigation drawer, Logout button moved from buyerActivity view to navigation drawer. BuyerBacker getInstance() is now public static
    
    * Added Edit Profile info functionality, added setters to ProfileSingleton. Added fields for profile picture URL when sending JSON on login
    
    * Merged seller_activity
    
    * Communication with editVenmo endpoint.
    
    * Added function to represent user as a JSON object
    
    * Generated SellerActivity java skeleton.
    Renamed seller_activity to activity_seller
    
    * Accidentally didn't rename activity_seller in the last commit even
    though I said I did.
    
    * Added Seek color changer
    
    * Commit before pull
    
    * Commit before pull
    
    * Added max and color to sliders
    
    * Moved signin source from buyerbacker to profileSingleton
    
    * Removed intent from Buyer Activity onCreate.
    Linked SellerActivity to BuyerActivity
    
    * Added slider functionality and a display
    
    * Added textviews
    
    * Price bar should display correctly
    
    * Price bar should display correctly
    
    * Should be last one
    
    * Added step to seekbar
    
    * Commit before pulling
    
    * Added time bar
    
    * Commit before pulling
    
    * Fixed max min value
    
    * Commit before pulling
    
    * Whoop
    
    * Swapped order of setMax and findviewbyID
    
    * ADDED PROFILE PICTURE :)))))
    Cleaned up Login logic
    
    * Done with work day
    
    * Moved testing files to dummyactivity
    
    * Commit before pull
    
    * Created Android InstrumentedTests
    
    * Added silent sign in for Facebook
    
    * Added default profile picture. Minor edits to Drawer and editProfile UI.
    
    * Completed Seller Activity
    
    * Combined activites
    
    * Sellers can post offers and buyers can search for offers.
    
    * Fixed result replication bug. Added average offer Textview. Still todo: fix unsubscribe average bug

--- frontend/myApplication/login/src/main/java/com/example/myapplication/Seller/SellerActivity.java
+++ frontend/myApplication/login/src/main/java/com/example/myapplication/Seller/SellerActivity.java
@@ -59,6 +59,8 @@ public class SellerActivity extends DrawerBaseActivity {
     LocalDate today = LocalDate.now();
     List<Boolean> diningHalls;
 
+    long average_price = 800;
+
     ResultBacker results;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -199,7 +201,8 @@ public class SellerActivity extends DrawerBaseActivity {
         networkManager.subscribe("/user/queue/sellerUpdate", new PostResponder());
         networkManager.subscribe("/user/queue/buyerFind", new FindOfferResponder());
         networkManager.subscribe("/user/queue/sellerInterest", new ConcreteNetworkResponder());
-
+        //TODO: FIX UNSUBSCRIBE ERROR
+        networkManager.subscribe("/topic/average", new AverageOfferResponder());
     }
 
     LocalDateTime convertTime(int hour, int minute)
@@ -211,6 +214,8 @@ public class SellerActivity extends DrawerBaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        //networkManager.subscribe("/topic/average", new AverageOfferResponder());
+        networkManager.send("/swipr/averageOffer");
         if (seller_flag) {
             sellerFrame.setVisibility(View.VISIBLE);
             buyerFrame.setVisibility(View.GONE);
@@ -276,6 +281,7 @@ public class SellerActivity extends DrawerBaseActivity {
     public class PostResponder implements NetworkResponder {
         public void onMessageReceived(String json)
         {
+            Log.d("Post Received: ", json);
             runOnUiThread(new Runnable() {
                 public void run() {
                     Toast toast = Toast.makeText(getApplicationContext(), "Swipe Offer Posted! We'll notify you when you've been matched.", Toast.LENGTH_LONG);
@@ -289,6 +295,7 @@ public class SellerActivity extends DrawerBaseActivity {
     public class FindOfferResponder implements NetworkResponder {
         public void onMessageReceived(String json)
         {
+            Log.d("Find Offer Received: ", json);
             Log.d("Received", json);
             try {
                 JSONArray jsonarray = new JSONArray(json);
@@ -306,6 +313,15 @@ public class SellerActivity extends DrawerBaseActivity {
     }
 
 
+    private class AverageOfferResponder implements NetworkResponder {
+        public void onMessageReceived(String value)
+        {
+            Log.d("Average Received: ", value);
+            average_price = Long.valueOf(value);
+            TextView average=findViewById(R.id.average_value);
+            average.setText("$" + String.format("%.2f", Long.valueOf(average_price) / (float) 100));
+        }
+    }
 }
 
 
