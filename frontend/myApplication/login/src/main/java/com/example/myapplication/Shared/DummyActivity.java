0512b0ea0a5af360cd7c31e57f4ecbbbfc337a20
Chris <cinokuchi@gmail.com>
2019-11-29 16:31:22 -0800

    Changed DummyActivity.java and dummy.xml to test out the service code. Added some code in SellerActivity to launch the dummy activity. Added some preliminary Service code in two files: SocketService.java and SocketServiceConnection.java

--- frontend/myApplication/login/src/main/java/com/example/myapplication/Shared/DummyActivity.java
+++ frontend/myApplication/login/src/main/java/com/example/myapplication/Shared/DummyActivity.java
@@ -1,23 +1,41 @@
 package com.example.myapplication.Shared;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.myapplication.R;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 public class DummyActivity extends AppCompatActivity {
 
     NetworkManager networkManager;
     ProfileSingleton profile;
+    public SocketService mService;
 
+    Button btnStartService, btnStopService, btnBindService, btnTestService;
+
+    SocketServiceConnection mServiceConnection;
+    public boolean mServiceBound = false;
+
+    @Override
+    protected void onStop() {
+        if(mServiceBound) {
+            unbindService(mServiceConnection);
+            mServiceBound = false;
+        }
+        super.onStop();
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,69 +46,76 @@ public class DummyActivity extends AppCompatActivity {
 
         setContentView(R.layout.dummy);
 
-
-        networkManager.subscribe("/user/queue/error", new TempResponder(R.id.textView5));
-        networkManager.subscribe("/topic/average", new TempResponder(R.id.textView6));
-        networkManager.subscribe("/user/queue/buyerFind", new TempResponder(R.id.textView10));
-        networkManager.subscribe("/user/queue/buyerInterest", new TempResponder(R.id.textView8));
-        networkManager.subscribe("/user/queue/sellerUpdate", new TempResponder(R.id.textView9));
-        networkManager.subscribe("/user/queue/sellerInterest", new TempResponder(R.id.textView12));
-        networkManager.subscribe("/user/queue/sellerCancel", new TempResponder(R.id.textView11));
+        btnStartService = findViewById(R.id.buttonStartService);
+        btnStopService = findViewById(R.id.buttonStopService);
+        btnBindService = findViewById(R.id.buttonBindService);
+        btnTestService = findViewById(R.id.buttonTestService);
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService();
+            }
+        });
+        btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService();
+            }
+        });
+        btnBindService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bind();
+            }
+        });
+        btnTestService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test();
+            }
+        });
     }
 
-    public void updateOffer(View view) {
-        Offer offer = new Offer();
-        offer.diningHallList = new ArrayList<>();
-        offer.diningHallList.add(true);
-        offer.diningHallList.add(false);
-        offer.diningHallList.add(false);
-        offer.diningHallList.add(false);
-        offer.userId = profile.getID();
-        offer.startTime = LocalDateTime.now();
-        offer.endTime = offer.startTime.plusHours(2);
-        offer.price = 420;
-        networkManager.send("/swipr/updateOffer", offer.generateQuery());
+    public void startService() {
+        Intent serviceIntent = new Intent(this, SocketService.class);
+        serviceIntent.putExtra("inputExtra", "Foreground Service Example in Android");
+        ContextCompat.startForegroundService(this, serviceIntent);
     }
 
-    public void findOffers(View view) {
-        Offer offer = new Offer();
-        offer.diningHallList = new ArrayList<>();
-        offer.diningHallList.add(true);
-        offer.diningHallList.add(false);
-        offer.diningHallList.add(false);
-        offer.diningHallList.add(false);
-        offer.userId = profile.getID();
-        offer.startTime = LocalDateTime.now();
-        offer.endTime = offer.startTime.plusHours(1);
-        offer.price = 421;
-        networkManager.send("/swipr/findOffers", offer.generateQuery());
+    public void stopService() {
+        Intent serviceIntent = new Intent(this, SocketService.class);
+        stopService(serviceIntent);
     }
 
-    public void cancelOffer(View view) {
-        networkManager.send("/swipr/cancelOffer");
+    void bind() {
+        mServiceConnection = new SocketServiceConnection();
+        Intent bindIntent = new Intent(this, SocketService.class);
+        bindService(
+                bindIntent,
+                mServiceConnection,
+                BIND_AUTO_CREATE);
     }
 
-    public void resetViews(View view) {
-        ((TextView) findViewById(R.id.textView5)).setText("here");
-        ((TextView) findViewById(R.id.textView6)).setText("here");
-        ((TextView) findViewById(R.id.textView11)).setText("here");
-        ((TextView) findViewById(R.id.textView8)).setText("here");
-        ((TextView) findViewById(R.id.textView9)).setText("here");
-        ((TextView) findViewById(R.id.textView10)).setText("here");
-        ((TextView) findViewById(R.id.textView12)).setText("here");
+    void test() {
+        if(mService.getTest())
+            btnTestService.setText("true");
+        else
+            btnTestService.setText("false");
     }
 
-    class TempResponder implements NetworkResponder {
-        TextView textView;
+    private class SocketServiceConnection implements ServiceConnection
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            SocketService.SocketBinder binder = (SocketService.SocketBinder) service;
+            mService = binder.getService();
+            mServiceBound = true;
 
-        public TempResponder(int id) {
-            textView = findViewById(id);
         }
 
         @Override
-        public void onMessageReceived(String json) {
-            textView.setText(json);
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
         }
     }
-
 }
