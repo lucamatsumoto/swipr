d3053a2c0f7db3d515d7a0bb1e1d5234096027e1
tholt1220 <trevholt1220@gmail.com>
2019-12-02 01:01:46 -0800

    added UI functionality to here view

--- frontend/myApplication/login/src/main/java/com/example/myapplication/Shared/HereActivity.java
+++ frontend/myApplication/login/src/main/java/com/example/myapplication/Shared/HereActivity.java
@@ -7,25 +7,74 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class HereActivity extends AppCompatActivity {
 
     private Button hereButton;
     private NetworkManager networkManager = NetworkManager.getInstance();
+    String fullName, venmo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String userJsonString = getIntent().getStringExtra("UserJSON");
+        JSONObject userJson = dummyUserJSON();
+        String DiningHall = getDiningHallString(getIntent().getLongExtra("DiningHall", 0));
+        long longValue = getIntent().getLongExtra("Time", 0);
+        LocalDateTime time =
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(longValue), ZoneId.systemDefault());
+
+
+        try
+        {
+            fullName = userJson.getString("firstName") + " " + userJson.getString("lastName");
+            venmo = userJson.getString("venmo");
+        }
+        catch(JSONException e)
+        {
+            Log.e("JSON Here Error", e.getMessage());
+        }
+
         setContentView(R.layout.activity_here);
         subscribeToHereTopic();
         hereButton = findViewById(R.id.here_button);
+
+        TextView nameView = findViewById(R.id.name_tag);
+        nameView.setText(fullName);
+
+        TextView timeView = findViewById(R.id.time_value);
+        timeView.setText(time.toString());
+
+
+    }
+
+    private String getDiningHallString(long bitField)
+    {
+        switch((int) bitField)
+        {
+            case 1:
+                return "Bruin Plate";
+            case 2:
+                return "Covel";
+            case 3:
+                return "De Neve";
+            case 4:
+                return "Feast";
+        }
+
+        return null;
     }
 
     private void subscribeToHereTopic() {
@@ -47,8 +96,8 @@ public class HereActivity extends AppCompatActivity {
             json.put("firstName", "Luca");
             json.put("lastName", "Matsumoto");
             json.put("email", "luca@matsumoto.us");
-            json.put("profilePicUrl", null);
-            json.put("venmo", null);
+            json.put("profilePicUrl", "https://lh3.googleusercontent.com/a-/AAuE7mDxNb7wKqRjR-1YZTgCy17m_3AyonadpYsFNxYgCw");
+            json.put("venmo", "@test");
         }
         catch(JSONException e)
         {
